fdb9f83113c2ef839465cb42eab620724bb6f24e
ChristopherTsh <christophertshoma@gmail.com>
2024-06-12 20:06:19 +0200

    "Added react-router-dom and Layout component to App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,20 @@
 import React from 'react'
+import {BrowerRouter, Routes, Route} from "react-router-dom";
+import Layout from './components/Layout';
 
+export default function App(){
+
+  return(
+    <BrowerRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      
+      
+      
+    </Route>
+    </Routes>
+    </BrowerRouter>
+
+
+  )
+}

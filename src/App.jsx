5ef96a64c1712f52d4d57aa75e58f05561e8d8c7
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-15 16:34:44 +0200

    "Removed podcast app code and replaced with React + Vite template, updated package.json and package-lock.json, and modified App.jsx and index.css files."

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {BrowerRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from './components/Layout';
 import Api from './components/Api/Api';
 
 export default function App(){
 
   return(
-    <BrowerRouter>
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
       <Route index element={<Api />} />
@@ -15,7 +15,7 @@ export default function App(){
       
     </Route>
     </Routes>
-    </BrowerRouter>
+    </BrowserRouter>
 
 
   )
---
+++ src/components/Api/ApiLayout.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Api from "./Api";
+
+export default function ApiLayout(){
+
+    const [post, setPosts] = useState([]);
+    const [error, setError ] = useState(null);
+
+    if(error){
+        return <div className="Error">{error}</div>;
+    }
+
+    return (
+        <div className="app-container">
+        <Api setPosts={setPosts} setError={setError} />
+        <h1>Api Layout</h1>
+        <dd className="centered-list">
+            {post.map((post) => (
+                <dd key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+        </dd>
+            ))}
+        </dd>
+        </div>
+    );
+}

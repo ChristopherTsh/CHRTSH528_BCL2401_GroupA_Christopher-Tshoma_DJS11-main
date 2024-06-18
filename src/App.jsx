50cbd65ec2f54b7b9add893b3870fc94a4d31f7f
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-18 10:48:42 +0200

    "Refactored App.jsx and index.css files: updated imports, removed unused code, and reformatted styles."

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Header from './Header/Header';
-import ApiLayout from './components/Api/ApiLayout';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import ApiLayout from "./components/Api/ApiLayout";
 // import Footer from './components/Footer';
-import Home from './Home';
-import Sidebar from './Sidebar/Sidebar';
+// import Home from './Home';
+import Sidebar from "./Sidebar/Sidebar";
+import './App.css'
 
-export default function App(){
-
-  return(
+export default function App() {
+  return (
     <Router>
-      <Header />
-      <Sidebar />
-    <Routes>
-      <Route path="/" element={<ApiLayout />} />
-      {/* <Route path="/sidebar" element={<Sidebar />} /> */}
-      {/* <Route path='/ApiLayout.jsx' element={<ApiLayout />} /> */}
-      {/* <Route path='footer' element={<Footer />} /> */}
+      <div className="app">
+        <Header />
+        <div className="app-body">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ApiLayout />} />
+            {/* <Route path="/sidebar" element={<Sidebar />} /> */}
+            {/* <Route path='/ApiLayout.jsx' element={<ApiLayout />} /> */}
+            {/* <Route path='footer' element={<Footer />} /> */}
 
-      
-      {/* <Route index element={<Api />} /> */}
-      
-      
-      
-    {/* </Route> */}
-    </Routes>
+            {/* <Route index element={<Api />} /> */}
+          </Routes>
+        </div>
+      </div>
     </Router>
-
-
-  )
+  );
 }

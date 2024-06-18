94affafe9ab9b414d8748c678d12d97fd496a019
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-18 10:05:01 +0200

    "Removed Header.jsx file and imported Header from './Header/Header' in App.jsx, added Header and Sidebar components to App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-// import Layout from './components/Layout';
+import Header from './Header/Header';
 // import ApiLayout from './components/Api/ApiLayout';
 // import Footer from './components/Footer';
 import Home from './Home';
@@ -10,8 +10,10 @@ export default function App(){
 
   return(
     <Router>
+      <Header />
+      <Sidebar />
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} /> */}
       <Route path="/sidebar" element={<Sidebar />} />
       {/* <Route path='/ApiLayout.jsx' element={<ApiLayout />} /> */}
       {/* <Route path='footer' element={<Footer />} /> */}
---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header() {
+  return ( <header className="header">
+   <h1>Hello Im Header</h1> 
+    </header>
+  );
+}
